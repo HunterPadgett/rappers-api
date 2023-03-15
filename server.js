a67cae5bd0da9fb36aa8c23def237182dd6f42cf
Hunter Padgett <gizmokabanjo@gmail.com>
2023-03-15 17:22:13 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const PORT = 6969;
+
+const rappers = {
+ "21 savage": {
+  age: 29,
+  birthName: "Sheyaa Bin Abraham-Joseph",
+  birthLocation: "London, England",
+ },
+ "chance the rapper": {
+  age: 29,
+  birthName: "Chancelor Bennett",
+  birthLocation: "Chicago, Illinois",
+ },
+ dylan: {
+  age: 29,
+  birthName: "Dylan",
+  birthLocation: "Dylan",
+ },
+};
+
+app.get("/", (req, res) => {
+ res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:rapper", (req, res) => {
+ const rapper = req.params.rapper.toLowerCase();
+ if (rappers[rapper]) res.json(rappers[rapper]);
+ else res.json(rappers["dylan"]);
+});
+
+app.listen(process.env.PORT || PORT, () => {
+ console.log(`running on http://localhost:${PORT}`);
+});
