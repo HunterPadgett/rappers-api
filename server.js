951f4891ee2329026eb717e06ce788c64486d5cd
Hunter Padgett <gizmokabanjo@gmail.com>
2023-03-18 18:06:15 -0400

    connected to db and ejs working

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 const PORT = 6969;
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+const connectionString = process.env.DB_CONNECTION_STRING;
 
-app.use(cors());
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+ (client) => {
+  const db = client.db("star-wars-crud");
+  const collection = db.collection("rappers");
 
-const rappers = {
- "21 savage": {
-  age: 29,
-  birthName: "Sheyaa Bin Abraham-Joseph",
-  birthLocation: "London, England",
- },
- "chance the rapper": {
-  age: 29,
-  birthName: "Chancelor Bennett",
-  birthLocation: "Chicago, Illinois",
- },
- dylan: {
-  age: 29,
-  birthName: "Dylan",
-  birthLocation: "Dylan",
- },
-};
+  app.set("view engine", "ejs");
+  app.use(cors());
+  app.use(express.static("public"));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.get("/", (req, res) => {
- res.sendFile(__dirname + "/index.html");
-});
+  app.get("/", (req, res) => {
+   collection
+    .find()
+    .toArray()
+    .then((data) => {
+     res.render("index.ejs", { info: data });
+    });
+  });
 
-app.get("/api/:rapper", (req, res) => {
- const rapper = req.params.rapper.toLowerCase();
- if (rappers[rapper]) res.json(rappers[rapper]);
- else res.json(rappers["dylan"]);
-});
+  // app.get("/api/:rapper", (req, res) => {
 
-app.listen(process.env.PORT || PORT, () => {
- console.log(`running on http://localhost:${PORT}`);
-});
+  // });
+
+  app.listen(process.env.PORT || PORT, () => {
+   console.log(`running on http://localhost:${PORT}`);
+  });
+ }
+);
