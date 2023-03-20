3ac7c9a5b17635c5d871404c6f98b32b3f741772
Hunter Padgett <gizmokabanjo@gmail.com>
2023-03-20 16:14:51 -0400

    testing render host

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 const connectionString = process.env.DB_CONNECTION_STRING;
 
+app.set("view engine", "ejs");
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
  (client) => {
   const db = client.db("star-wars-crud");
   const collection = db.collection("rappers");
-
-  app.set("view engine", "ejs");
-  app.use(cors());
-  app.use(express.static("public"));
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
 
   app.get("/", (req, res) => {
    collection
